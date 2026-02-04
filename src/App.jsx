9f203c079a234266d663a4da579806d8f9af669c
TUM-08 <arthur.nsp.tum@gmail.com>
2026-02-04 08:03:35 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <div className="auth-container">
+        <div className="auth-background">
+          <div className="gradient-orb-1"></div>
+          <div className="gradient-orb-2"></div>
+          <div className="gradient-orb-3"></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
